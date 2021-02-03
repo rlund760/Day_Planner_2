31bdbcdcd5fa6128815e46b169fc705d64affbcf
rlund760 <robdlundberg@gmail.com>
2021-02-03 15:21:49 -0800

    added local storage

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 $(".btn-primary").on("click",function(){
     var id=$(this).attr("id")
-    console.log (id)
+    var timeblock=id.split("-")[0]
+    var textInput=$(`#${timeblock}-planner`).val()
+    console.log (id,timeblock,textInput)
+    localStorage.setItem(timeblock,textInput)
 })
