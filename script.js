f79c320de4c8970971a0b903d30c0ada912a9378
rlund760 <robdlundberg@gmail.com>
2021-02-03 18:01:45 -0800

    final product

--- script.js
+++ script.js
@@ -5,7 +5,17 @@ $(".btn-primary").on("click",function(){
     console.log (id,timeblock,textInput)
     localStorage.setItem(timeblock,textInput)
 })
+var currenthour = moment().hours()
+console.log(currenthour)
+$("#today").text(moment())
 for(let i=8; i<18;i++){
     var localStorageData=localStorage.getItem(i)
     $(`#${i}-planner`).val(localStorageData)
+    if(i>currenthour){
+        $(`#${i}-planner`).addClass("bg-info")
+    } else if(i=== currenthour){
+        $(`#${i}-planner`).addClass("bg-warning")
+    }else{
+        $(`#${i}-planner`).addClass("bg-danger")
+    }
 }
